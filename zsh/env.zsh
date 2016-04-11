b69c367cd0b2860efec1fcb027b0f64a49920c3b
Spencer Michaels <sxmichaels@gmail.com>
2016-04-11 13:57:20 -0700

    routine update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,26 +1,27 @@
 export EDITOR=vim
 
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin:"        # The usual
+eval `/usr/libexec/path_helper -s`                  # Tex paths
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"  # Standard homebrew paths
-export PATH="/usr/texbin:$PATH"                     # MacTeX binaries
-export PATH="$HOME/Library/Haskell/bin:$PATH"       # Haskell binaries
-export PATH="$HOME/.local/bin:$PATH"                # pipsi
 
-export PATH="$HOME/devtools/android-sdks/platform-tools" # ADB
+export PATH="$HOME/devtools/android-sdks/platform-tools:$PATH" # ADB
 export ANDROID_HOME=/usr/local/opt/android-sdk      # Android SDK
+
+# Brewed Mac Emacs
+export PATH="/usr/local/Cellar/emacs-mac/emacs-24.5-z-mac-5.15/bin:$PATH"
 
 export MANPATH="/usr/local/man:$MANPATH"
 
-eval `/usr/libexec/path_helper -s`                  # Tex paths
-
+# The current version of gpg-agent causes errors in ssh-agent, so the following
+# is disabled for now. See https://bugzilla.redhat.com/show_bug.cgi?id=1274245.
 # On Mac, load GPG agent information into env vars
-if [[ $(uname) == Darwin ]]; then
-    if [ -f "${HOME}/.gpg-agent-info" ]; then
-        . "${HOME}/.gpg-agent-info"
-        export GPG_AGENT_INFO
-        export SSH_AUTH_SOCK
-    fi
-fi
+#if [[ $(uname) == Darwin ]]; then
+#    if [ -f "${HOME}/.gpg-agent-info" ]; then
+#        . "${HOME}/.gpg-agent-info"
+#        export GPG_AGENT_INFO
+#        export SSH_AUTH_SOCK
+#    fi
+#fi
 
 # Tell pinentry where to prompt for the password
-export GPG_TTY=$(tty)
+#export GPG_TTY=$(tty)
--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,11 +1,11 @@
 # oh-my-zsh configuration
 ZSH=$HOME/.oh-my-zsh
 
-plugins=(tmux osx brew sudo git autojump last-working-dir)
+plugins=(osx brew sudo git autojump last-working-dir)
 
 # Plugin: tmux ----------------------------------------------------------------
-ZSH_TMUX_AUTOSTART=false
-ZSH_TMUX_AUTOSTART_ONCE=true
-ZSH_TMUX_AUTOCONNECT=false
-ZSH_TMUX_AUTOQUIT=true
-ZSH_TMUX_FIXTERM=true
+#ZSH_TMUX_AUTOSTART=false
+#ZSH_TMUX_AUTOSTART_ONCE=true
+#ZSH_TMUX_AUTOCONNECT=false
+#ZSH_TMUX_AUTOQUIT=true
+#ZSH_TMUX_FIXTERM=true
